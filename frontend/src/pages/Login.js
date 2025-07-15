a577e536f3623f96b00e2333bf4885c424ef69b8
chlsey <chelseywang0219@gmail.com>
2025-07-15 16:16:14 -0400

    Frontend Accessibility Updates Login Page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -139,8 +139,12 @@ const Login = () => {
             </div>
 
             <div className="text-center text-sm">
-              <Link to="/register" className="text-gray-400 hover:text-white transition-colors">
-                Don't have an account? Sign up
+              <div className="text-gray-400 mb-2">Don't have an account?</div>
+              <Link 
+                to="/register" 
+                className="inline-block w-full rounded bg-white px-3 py-2 text-sm font-semibold text-black hover:bg-gray-200 transition-colors focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-black"
+              >
+                Sign up
               </Link>
             </div>
           </form>
@@ -150,4 +154,4 @@ const Login = () => {
   );
 };
 
-export default Login; 
+export default Login;
